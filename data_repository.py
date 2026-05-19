e3dbc9eb3622c700b794d930f0c293b11ff20ed6
Harry Smith <harryjsmith40@gmail.com>
2026-05-19 16:16:09 +1000

    refactor: extract data access layer into DataRepository class

---
+++ data_repository.py
@@ -0,0 +1,58 @@
+from Config.config import master_record_path, account_info_path, schema
+
+import logging
+import pandas as pd
+
+class DataRepository():
+    '''Handles all data file interactions - while class is not strictly needed at this stage is exists for when a DB is implemented'''
+    schema = schema
+
+    @staticmethod
+    def read_master():
+        '''Reads the master file'''
+        master_record = pd.read_csv(master_record_path, dtype=schema['dtypes'], parse_dates=schema['date_columns'], date_format=schema['date_format'])
+        return master_record
+
+    @staticmethod
+    def to_master(df):
+        '''Updates master_record.csv to include new data'''
+        # Appends to master_record.csv the new data
+        df.to_csv(master_record_path, mode='a', header=False, index=False, date_format=schema['date_format'])
+        logging.info('Successfully appended to master')
+
+    @staticmethod
+    def write_master(master_record):
+        master_record.to_csv(master_record_path, index=False)
+        logging.info('Successfully writen to master')
+
+    @staticmethod
+    def read_account():
+        '''Reads the accounts CSV into a dataframe'''
+        account_info = pd.read_csv(account_info_path)
+        return account_info
+    
+    @staticmethod
+    def update_timestamp(selection, account_info):
+        # Updated the last updated timestamp on the accounts CSV
+        account_info.loc[selection,'Last Updated'] = str(pd.Timestamp.now())
+        account_info.to_csv(account_info_path, index=False)
+        logging.info('Timestamp successfully updated')
+
+    @staticmethod
+    def create_account(account_name, account_type):
+        '''Creates a new account in the account file'''
+        new_account_info = pd.DataFrame({'Account Name': [account_name], 'Account Type': [account_type], 'Last Updated': [pd.Timestamp.now()]})
+        new_account_info.to_csv(account_info_path, mode='a', header=False, index=False)
+
+    @staticmethod
+    def write_account():
+            '''Creates the accounts file when it doesn't exist'''
+            account_info_df = pd.DataFrame(columns=['Account Name', 'Account Type', 'Last Updated'])
+            account_info = account_info_df.to_csv(account_info_path, index=False)
+            return account_info_df
+
+
+    @staticmethod
+    def read_input_CSV(file_path):
+        df = pd.read_csv(file_path, names=['Date', 'Amount', 'Desc', 'Balance'], header=None, dtype=schema['input_dtypes'], parse_dates=schema['date_columns'], date_format=schema['date_format'])
+        return df
--- financial_tracker.py
+++ financial_tracker.py
@@ -1,4 +1,5 @@
 from Config.config import schema, data_folder, master_record_path, account_info_path
+from data_repository import DataRepository
 
 import os
 import pandas as pd
@@ -15,45 +16,37 @@ class FinancialTracker:
         # Currently assumes ./Data/ exists if the user doesn't have ./Data/master_record.csv 
         if os.path.exists(master_record_path):
             # Checks structure of file is correct
-            master_record = pd.read_csv(master_record_path)
+            master_record = DataRepository.read_master()
             if master_record.columns.tolist() == ['Date', 'Amount', 'Desc', 'Balance', 'Account Name', 'Account Type']:
                 logging.info('master_record initialised correctly')
             else: 
                 logging.error('master_record exists with incorrect headers')
                 raise ValueError('master_record exists with incorrect headers')
-            
+
         # Creates it if it doesn't exist
         else:
             master_record = pd.DataFrame(columns=['Date', 'Amount', 'Desc', 'Balance', 'Account Name', 'Account Type'])
             if os.path.exists(data_folder):
-                master_record.to_csv(master_record_path, index=False)
+                DataRepository.write_master(master_record)
             else:
                 os.mkdir(data_folder)
-                master_record.to_csv(master_record_path, index=False)
+                DataRepository.write_master(master_record)
 
         # Checks to see if the accounts CSV exists
         if os.path.exists(account_info_path):
-            self.account_info = pd.read_csv(account_info_path)
+            self.account_info = DataRepository.read_account()
             if self.account_info.columns.tolist() == ['Account Name', 'Account Type', 'Last Updated']:
-                logging.debug('account_info initialised correctly')
+                logging.info('account_info initialised correctly')
 
         # Creates the account CSV if it doesn't
         else:
-            account_info_df = pd.DataFrame(columns=['Account Name', 'Account Type', 'Last Updated'])
-            account_info_df.to_csv(account_info_path, index=False)
-            self.account_info = account_info_df
-
-    def read_master(self):
-        '''Reads the master file'''
-        master_record = pd.read_csv(master_record_path, dtype=self.schema['dtypes'], parse_dates=self.schema['date_columns'], date_format=self.schema['date_format']
-)
-        return master_record
+            self.account_info = DataRepository.write_account()
 
     def read_and_clean(self, file_path):
         '''Reads CSV files, formates dates and data types, removes null data and adds an index'''
         # Read the CSV file into a DataFrame
-        df = pd.read_csv(file_path, names=['Date', 'Amount', 'Desc', 'Balance'], header=None, dtype=self.schema['input_dtypes'], parse_dates=self.schema['date_columns'], date_format=self.schema['date_format']
-)
+        df = DataRepository.read_input_CSV(file_path)
+
         # Drops na values if the amount or date or balance is missing
         df.dropna(subset=['Date','Amount','Balance'], inplace=True)
 
@@ -70,28 +63,12 @@ class FinancialTracker:
     
     def deduplicate(self, cleaned_input):
         ''' Checks for and removes duplicates''' 
-        master_record = self.read_master()
+        master_record = DataRepository.read_master()
         merged_df = cleaned_input.merge(master_record, how='left', on=['Date', 'Amount', 'Desc', 'Balance', 'Account Name', 'Account Type'], indicator=True)
         merged_df = merged_df[merged_df['_merge'] == 'left_only']
         merged_df = merged_df.drop(labels='_merge' , axis='columns')
 
         return merged_df
-
-    def to_master(self, df):
-        '''Updates master_record.csv to include new data'''
-        # Appends to master_record.csv the new data
-        df.to_csv(master_record_path, mode='a', header=False, index=False, date_format=self.schema['date_format'])
-    
-    def create_account(self):
-        '''Creates a new account'''
-        account_name = input('Please provide the name of the account this data is from: ')
-        account_type = input('Please provide the type of this account (e.g. current, savings, credit card): ')
-
-        # Updates the last updated timestamp in the accounts file 
-        new_account_info = pd.DataFrame({'Account Name': [account_name], 'Account Type': [account_type], 'Last Updated': [pd.Timestamp.now()]})
-        new_account_info.to_csv(account_info_path, mode='a', header=False, index=False)
-
-        return account_name, account_type
 
     def select_account(self):
         
@@ -102,21 +79,26 @@ class FinancialTracker:
             print('A Add New Account')
             print('B Back to main menu')
 
-            self.account_name_option = input('Please provide the number/letter of the account this data is from: ')
+            account_name_option = input('Please provide the number/letter of the account this data is from: ')
 
             # Creates new account
-            if self.account_name_option.upper() == 'A':
-                account_name, account_type =  self.create_account()
+            if account_name_option.upper() == 'A':
+                # User provides account name and type
+                account_name = input('Please provide the name of the account this data is from: ')
+                account_type = input('Please provide the type of this account (e.g. current, savings, credit card): ')
+
+                DataRepository.create_account(account_name, account_type)
+
                 return account_name, account_type
             
             # Goes back to main menu
-            elif self.account_name_option.upper() == 'B':
+            elif account_name_option.upper() == 'B':
                 break
             
             else:
                 # Checks if selected option is a valid account number 
                 try:
-                    selection = int(self.account_name_option)
+                    selection = int(account_name_option)
                 except ValueError:
                     print('Invalid input please try again')
                     logging.info('Select account - Invalid input please try again')
@@ -124,36 +106,38 @@ class FinancialTracker:
 
                 # Checks if number falls within the range of existing accounts
                 if selection < len(self.account_info):
-                    # Updated the last updated timestamp on the accounts CSV
-                    self.account_info.loc[selection,'Last Updated'] = str(pd.Timestamp.now())
-                    self.account_info.to_csv(account_info_path, index=False)
-
+                    account_info = DataRepository.read_account()
                     # Reads the account details and returns them
-                    account_name = self.account_info.loc[selection].iat[0]
-                    account_type = self.account_info.loc[selection].iat[1]
+                    account_name = account_info.loc[selection, 'Account Name']
+                    account_type = account_info.loc[selection, 'Account Type']
+
+                    # Updates the timestamp of the account updated to
+                    DataRepository.update_timestamp(selection, account_info)
+
                     return account_name, account_type
 
                 # Fall back for invalid input
                 else:
                     print('Invalid input please try again')
+                    logging.error('Select account - Invalid input please try again')
 
     def upload_file(self):
         '''Prompts the user for the file name'''
         
         while True:
             print('B Back to main menu')
-            self.file_path_option = input('Please ensure the CSV file is in the data folder and provide the file name would like to upload: ')
+            file_path_option = input('Please ensure the CSV file is in the data folder and provide the file name would like to upload: ')
 
             # Sends user back to main menu if selected
-            if self.file_path_option.upper() == 'B':
+            if file_path_option.upper() == 'B':
                 break
             
             # Uses the schema file paths to create the full file path for the CSV
-            self.file_path = os.path.join(data_folder, self.file_path_option)
+            file_path = os.path.join(data_folder, file_path_option)
 
             # Checks if the provided file/path exists
-            if os.path.exists(self.file_path):
-                cleaned_input = self.read_and_clean(self.file_path)
+            if os.path.exists(file_path):
+                cleaned_input = self.read_and_clean(file_path)
                 logging.info('file_path exists correctly')
 
                 # Handles case where user goes back to menu rather than continues
@@ -170,9 +154,9 @@ class FinancialTracker:
                 deduplicated_input = self.deduplicate(cleaned_input)
                 
                 # Writes the uploaded file to the master record
-                self.to_master(deduplicated_input)
+                DataRepository.to_master(deduplicated_input)
 
             # Raises issue if file doesn't exist
             else:
-                logging.info('Failed file does not exist')
+                logging.error('Failed file does not exist')
                 print('Upload file - Failed file does not exist')
